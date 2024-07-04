d341257e675162fb360298f88f03d2de99b51fb8
Jorge Ivan Sierra Suarez <50022849+jissbossh@users.noreply.github.com>
2024-07-04 09:42:46 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 class Program {
     static main() {
-       console.log("Hola My doctora Agie ❤😍😘!");
-       document.getElementById('titulo').innerHTML = "Hola My doctora Agie ❤😍😘!";
+       console.log("Hola My doctora Angie ❤😍😘!");
+       document.getElementById('titulo').innerHTML = "Hola My doctora Angie ❤😍😘!";
       }
   }
   Program.main();
