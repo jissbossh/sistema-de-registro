ab25bd2cf8cca2c6d2aee0e45681b12ff5b6ec3c
Jorge Ivan Sierra Suarez <50022849+jissbossh@users.noreply.github.com>
2024-07-04 09:40:07 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+class Program {
+    static main() {
+       console.log("Hola My doctora Agie ❤😍😘!");
+       document.getElementById('titulo').innerHTML = "Hola My doctora Agie ❤😍😘!";
+      }
+  }
+  Program.main();
