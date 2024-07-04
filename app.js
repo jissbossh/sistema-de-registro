edee3126e48a2a437a13a133015f741d3c373109
Jorge Ivan Sierra Suarez <jorge7gt22@hotmail.com>
2024-07-04 15:38:21 -0500

    Add the directory sripts

--- app.js
+++
@@ -1,7 +0,0 @@
-class Program {
-    static main() {
-       console.log("Hola My doctora Angie ❤😍😘!");
-       document.getElementById('titulo').innerHTML = "Hola My doctora Angie ❤😍😘!";
-      }
-  }
-  Program.main();
---
+++ scripts/app.js
@@ -0,0 +1,7 @@
+class Program {
+    static main() {
+       console.log("Hola My doctora Angie ❤😍😘!");
+       document.getElementById('titulo').innerHTML = "Hola My doctora Angie ❤😍😘!";
+      }
+  }
+  Program.main();
